326ba90377ea099ce337790becbcff233400e56e
Maks <Crossfade143@gmail.com>
2021-04-15 16:11:34 +0300

    Create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Header,
   },
+  data: () => ({
+    firstName: 'Maxim Saviuk',
+    num: '2',
+  }),
 };
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+}
+
+body {
+  background-color: #151515;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #e0e0e0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header-logo">
+        <img  class="logo" src="../assets/MyLogo.png" alt="headerlogo">
+        <h4 class="author">Maxim Saviuk</h4>
+      </div>
+
+      <div class="header-menu">
+        <a class="menu--link" href="#">Про мене</a>
+        <a class="menu--link" href="#">Походи</a>
+        <a class="menu--link" href="#">Галерея</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.header {
+  width: 100vw;
+  height: 50px;
+  background-color: #1b2021;
+
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1620px;
+  margin: 0 auto;
+}
+
+.header-logo {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.author {
+  padding: 0 20px;
+}
+
+.logo {
+  margin: 5px 0;
+  height: 40px;
+  width: 40px;
+}
+
+.menu--link {
+  margin: 0 10px;
+  text-decoration: none;
+  color: aliceblue;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
